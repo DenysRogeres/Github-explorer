c09fc2fbf93725c8f810aca027fab07cfe9339f2
DenysRogeres <denysmex@gmail.com>
2021-06-19 23:32:47 -0300

    Adding link repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,23 @@
 import { RepositoryItem } from './RepositoryItem';
+import { useEffect, useState } from 'react';
+
 import '../styles/repositories.scss';
 
 const repository = {
-    nome: 'Repositório Denys',
-    description: 'Repositório com vários códigos',
-    link: 'https://github.com/unform/unform'
-
+    name: 'Denys',
+    description: 'Forms in React',
+    link: 'https://api.github.com/orgs/rocketseat/repos'
 }
+
 export function RepositoryList () {
+    const [repositories, setRespositories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+        .then(response => response.json())
+        .then(data => setRespositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
         <h1>Lista de repositórios</h1>
